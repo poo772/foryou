5b660a1d166673595c9037af5155ab1264948768
poo772 <bgsprstyo1@gmail.com>
2025-05-05 13:14:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function ganti() {
+  document.getElementById("pass").innerHTML = "Lopyuuu";
+}
+
+function ganti2() {
+  document.getElementById("pass").innerHTML = "👇Sebelah kiri aja, yang ini eror";
+}
